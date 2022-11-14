5e47a50d401ac1b212e4eb90beb84f8e62b1d3a6
Vando Diniz <vandocarlos2001@outlook.com>
2022-11-14 13:04:59 -0300

    ET BILU V1.1: Modularizado

---
+++ jogador.py
@@ -0,0 +1,61 @@
+class Jogador():
+    def __init__(self, nome, tier, convidados=[]):
+        self.nome = nome
+        self.tier = tier
+        self.convidados = convidados
+
+
+# TIER S
+thigas = Jogador('thigas', 40)
+
+# TIER A
+marcos = Jogador("marcos", 35)
+pani = Jogador("pani", 35)
+raissa = Jogador("raissa", 35)
+magnus = Jogador("magnus", 35)
+lucas = Jogador("lucas", 35)
+
+# TIER B
+leite = Jogador("leite", 30)
+vando = Jogador("vando", 30)
+bruno = Jogador("bruno", 30)
+devao = Jogador("devao", 30)
+pedro = Jogador("pedro", 30)
+
+# TIER C
+bitola = Jogador("bitola", 25)
+rato = Jogador("rato", 25)
+vinhal = Jogador("vinhal", 25)
+teteu = Jogador("teteu", 25)
+vargas = Jogador("vargas", 25)
+warley = Jogador("warley", 25)
+
+# TIER D
+rickao = Jogador("rickao", 20)
+bia = Jogador("bia", 20)
+thaliny = Jogador("thaliny", 20)
+markin = Jogador("markin", 20)
+ney = Jogador("ney", 20)
+litorate = Jogador("litorate", 20)
+sarah = Jogador("sarah", 20)
+malu = Jogador("malu", 20)
+
+# TIER E
+gabriel = Jogador("gabriel", 15)
+ale = Jogador("ale", 15)
+jensom = Jogador("jensom", 15)
+loloh = Jogador("loloh", 15)
+miguel = Jogador("miguel", 15)
+gira = Jogador("gira", 15)
+marques = Jogador("marques", 15)
+
+# TIER F
+rata = Jogador("rata", 5)
+gusthavo = Jogador("gusthavo", 5)
+poly = Jogador("poly", 5)
+fabao = Jogador("fabao", 5)
+
+# TIER Z
+moura = Jogador("moura", 0)
+igor = Jogador("igor", 0)
+giulia = Jogador("giulia", 0)
--- main.py
+++ main.py
@@ -1,86 +1,8 @@
-class Jogador():
-    def __init__(self, nome, tier, convidados=[]):
-        self.nome = nome
-        self.tier = tier
-        self.convidados = convidados
+import random
+import jogador
 
 
-# TIER S
-thigas = Jogador('thigas', 40)
-
-# TIER A
-marcos = Jogador("marcos", 35)
-pani = Jogador("pani", 35)
-raissa = Jogador("raissa", 35)
-magnus = Jogador("magnus", 35)
-lucas = Jogador("lucas", 35)
-
-# TIER B
-leite = Jogador("leite", 30)
-vando = Jogador("vando", 30)
-bruno = Jogador("bruno", 30)
-devao = Jogador("devao", 30)
-pedro = Jogador("pedro", 30)
-
-# TIER C
-bitola = Jogador("bitola", 25)
-rato = Jogador("rato", 25)
-vinhal = Jogador("vinhal", 25)
-teteu = Jogador("teteu", 25)
-vargas = Jogador("vargas", 25)
-warley = Jogador("warley", 25)
-
-# TIER D
-rickao = Jogador("rickao", 20)
-bia = Jogador("bia", 20)
-thaliny = Jogador("thaliny", 20)
-markin = Jogador("markin", 20)
-ney = Jogador("ney", 20)
-litorate = Jogador("litorate", 20)
-sarah = Jogador("sarah", 20)
-malu = Jogador("malu", 20)
-
-# TIER E
-gabriel = Jogador("gabriel", 15)
-ale = Jogador("ale", 15)
-jensom = Jogador("jensom", 15)
-loloh = Jogador("loloh", 15)
-miguel = Jogador("miguel", 15)
-gira = Jogador("gira", 15)
-marques = Jogador("marques", 15)
-
-# TIER F
-rata = Jogador("rata", 5)
-gusthavo = Jogador("gusthavo", 5)
-poly = Jogador("poly", 5)
-fabao = Jogador("fabao", 5)
-
-# TIER Z
-moura = Jogador("moura", 0)
-igor = Jogador("igor", 0)
-giulia = Jogador("giulia", 0)
-
-TAMANHO_TIME = 4
-NUM_TIMES = 3
-
-jogadores = [
-    vando,
-    thigas,
-    bruno,
-    markin,
-    rato,
-    teteu,
-    litorate,
-    warley,
-    marcos,
-    vargas,
-]
-
-thigas.convidados = [lucas]
-markin.convidados = [malu]
-
-
-def calcula_nota_de_corte():
+def calcula_nota_de_corte(jogadores):
     soma = 0
     for jogador in jogadores:
         soma += jogador.tier
@@ -90,17 +12,12 @@ def calcula_nota_de_corte():
     return soma
 
 
-IDEAL = calcula_nota_de_corte()
-
-import random
-
-
 def sorteia_jogador(nomes):
     pos = random.randint(0, len(nomes) - 1)
     return nomes[pos]
 
 
-def gera_times():
+def gera_times(TAMANHO_TIME, NUM_TIMES, jogadores):
     times = []
     lista_jogadores = jogadores.copy()
     for i in range(NUM_TIMES):
@@ -108,7 +25,7 @@ def gera_times():
         while True:
             jogador = sorteia_jogador(lista_jogadores)
 
-            if jogador.convidados == []:
+            if not jogador.convidados:
                 time.append(jogador)
                 lista_jogadores.remove(jogador)
 
@@ -135,20 +52,20 @@ def calcula_valor_final_time(time):
     return soma
 
 
-def calcula_score_time(time):
+def calcula_score_time(time, IDEAL):
     soma = calcula_valor_final_time(time)
     dif = (IDEAL - soma) ** 2
     return dif
 
 
-def calcula_score_solucao(times):
+def calcula_score_solucao(times, IDEAL):
     score = 0
     for time in times:
-        score += calcula_score_time(time)
+        score += calcula_score_time(time, IDEAL)
     return score
 
 
-def printa_times(times):
+def printa_times(times, IDEAL):
     print(f"IDEAL: {IDEAL}")
     print('-' * 30)
     for i in range(NUM_TIMES):
@@ -158,20 +75,50 @@ def printa_times(times):
         print('-' * 30)
 
 
+def checa_parametros(TAMANHO_TIME, NUM_TIMES, jogadores):
+    convidados = []
+    for jogador in jogadores:
+        for convidado in jogador.convidados:
+            convidados.append(convidado)
+
+    if len(convidados) + len(jogadores) != TAMANHO_TIME*NUM_TIMES:
+        raise Exception("Número de jogadores cadastrados incompatível com o selecionado! Revisar cenário.")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def sorteia_times(TAMANHO_TIME, NUM_TIMES, jogadores):
+    IDEAL = calcula_nota_de_corte(jogadores)
     score = melhor_score = 100000
     i = 0
     while i < 1000 and score != 0:
-        times_sorteados = gera_times()
-        score = calcula_score_solucao(times_sorteados)
+        times_sorteados = gera_times(TAMANHO_TIME, NUM_TIMES, jogadores)
+        score = calcula_score_solucao(times_sorteados, IDEAL)
         if score < melhor_score:
             melhor_score = score
             solucao = times_sorteados.copy()
 
         i += 1
-        print(i)
+    printa_times(solucao, IDEAL)
 
-    printa_times(solucao)
+
+if __name__ == '__main__':
+    # PARÂMETROS CUSTOMIZAVEIS
+    TAMANHO_TIME = 4
+    NUM_TIMES = 3
+    jogadores = [
+        jogador.vando,
+        jogador.thigas,
+        jogador.bruno,
+        jogador.markin,
+        jogador.rato,
+        jogador.teteu,
+        jogador.litorate,
+        jogador.warley,
+        jogador.marcos,
+        jogador.vargas,
+    ]
+    jogador.thigas.convidados = [jogador.lucas]
+    jogador.markin.convidados = [jogador.malu]
+
+    # OTIMIZADOR
+    checa_parametros(TAMANHO_TIME, NUM_TIMES, jogadores)
+    sorteia_times(TAMANHO_TIME, NUM_TIMES, jogadores)
